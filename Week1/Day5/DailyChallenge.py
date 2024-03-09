416f0d161ea73dc392d84b80fd46e9e1a49ab69d
zhanna-naumenko <jannetj88@gmail.com>
2024-03-09 23:31:19 +0200

    changed the first challenge, as I didn't see that it should be done with List Comprehension and changed the second challenge, as I saw it doesn't work properly: changed the symbol in line 17 and added len() function where it was necessary

--- Week1/Day5/DailyChallenge.py
+++ Week1/Day5/DailyChallenge.py
@@ -1,25 +1,20 @@
 #Challenge 1
 
 user_words = input("Please write the words separating them by coma: ")
-#deleted spaces
-user_words = user_words.replace(" ", "")
-#put every word in to the list
-user_list_of_words = list(user_words.split(","))
-#sorting the list
-user_list_of_words.sort()
-print(",".join(user_list_of_words))
+
+sorted_words = sorted([word.strip() for word in user_words.split(',')])
+print(",".join(sorted_words))
 
 #Challenge 2
 user_sentence = input("Please write your sentence: ")
 list_of_words_in_sentence = list(user_sentence.split(" "))
-print(list_of_words_in_sentence)
 
 def get_longest_word(list):
     '''Gets the longest word from the list'''
-    longest_item = len(list[0])
+    longest_item = list[0]
     for item in list:
-        if len(item) >= longest_item:
-            longest_item == item
-    return item
+        if len(item) >= len(longest_item):
+            longest_item = item
+    return longest_item
 
 print(get_longest_word(list_of_words_in_sentence))
