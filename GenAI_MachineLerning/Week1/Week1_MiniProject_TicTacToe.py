4f96289363013564e534bdf1fd6bd335fb9b6cde
Zhanna <75215351+zhanna-naumenko@users.noreply.github.com>
2026-02-19 10:40:15 +0200

    Update Week1_MiniProject_TicTacToe.py
    Сhanged the game board to make it easier to access variables. Now it's 2D list.

--- GenAI_MachineLerning/Week1/Week1_MiniProject_TicTacToe.py
+++ GenAI_MachineLerning/Week1/Week1_MiniProject_TicTacToe.py
@@ -1,76 +1,60 @@
 
-print("Welcome to Tic Tac Toe!")
+playing_board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
 
-first_row = [[" "],[" "],[" "]]
-second_row = [[" "],[" "],[" "]]
-third_row = [[" "],[" "],[" "]]
-playing_board = [first_row,second_row,third_row]
-
-def display_board():
+def display_board(board):
     """Displaying the board"""
-    tictactoe_board = f"""TIC TAC TOE
-    *********************
-    *    {first_row[0][0]}  |  {first_row[1][0]}  |  {first_row[2][0]}    *
-    *   ----|-----|----   *
-    *    {second_row[0][0]}  |  {second_row[1][0]}  |  {second_row[2][0]}    *
-    *   ----|-----|----   *
-    *    {third_row[0][0]}  |  {third_row[1][0]}  |  {third_row[2][0]}    *
-    *********************"""
-    print(tictactoe_board)
-# display_board()
+    board = f"""TIC TAC TOE
+*******************
+*    {board[0][0]} | {board[0][1]} | {board[0][2]}     *
+*   ___|___|___    *
+*    {board[1][0]} | {board[1][1]} | {board[1][2]}     *
+*   ___|___|___    *
+*    {board[2][0]} | {board[2][1]} | {board[2][2]}     *
+*      |   |       *  
+*******************"""
+    print(board)
 
 def player_input(player):
     """Puts player input into the board"""
-    user_row = int(input("Enter row number: "))
-    user_column = int(input("Enter column number: "))
-    playing_board[user_row - 1][user_column - 1][0] = player
+    user_row = int(input("Enter your row: "))
+    user_column = int(input("Enter your column: "))
+    playing_board[user_row - 1][user_column - 1] = player
+    return playing_board
 
-
-def check_win(board, player):
+def check_winner(board, player):
     """Checks if player won the game"""
-    if ((board[0][0][0]==player and board[0][1][0]== player and board[0][2][0]==player )or #for row1
-
-            (board[1][0][0]==player and board[1][1][0]==player and board[1][2][0]==player )or #for row2
-
-            (board[2][0][0]==player and board[2][1][0]==player and board[2][2][0]==player )or #for row3
-
-            (board[0][0][0]==player and board[1][0][0]==player and board[2][0][0]== player )or#for Colm1
-
-            (board[0][1][0]==player and board[1][1][0]==player and board[2][1][0]==player )or #for Colm 2
-
-            (board[0][2][0]==player and board[1][2][0]==player and board[2][2][0]==player )or #for colm 3
-
-            (board[0][0][0]==player and board[1][1][0]==player and board[2][2][0]==player )or #daignole 1
-
-            (board[0][2][0]==player and board[1][1][0]==player and board[2][0][0]==player )):#daignole 2
+    if ((board[0][0] == player and board[0][1] == player and board[0][2]== player) or #Row1
+    (board[1][0] == player and board[1][1] == player and board[1][2]== player) or #Row2
+    (board[2][0] == player and board[2][1] == player and board[2][2]== player) or #Row3
+    (board[0][0] == player and board[1][0] == player and board[2][0]== player) or #Column1
+    (board[0][1] == player and board[1][1] == player and board[2][1]== player) or #Column2
+    (board[0][2] == player and board[1][2] == player and board[2][2]== player) or #Column3
+    (board[0][0] == player and board[1][1] == player and board[2][2]== player) or #Diagonal1
+    (board[0][2] == player and board[1][1] == player and board[2][0]== player)):#Diagonal2
         return True
     return False
 
-
 def check_tie(board):
     """Checks if the game has resulted in a tie"""
-    # Check if there is any empty space left
     for row in board:
-        for cell in row:
-            if cell[0] == " ":
-                return False
+        if " " in row:
+            return False
     return True
-
 
 def play_game(board):
     """Play the game"""
     players = ["X", "O"]
-    display_board()
+    display_board(board)
     while True:
         for player in players:
-            print(f"Player's {player} turn...")
+            print(f"Player {player} tern...")
             player_input(player)
-            display_board()
-
-            if check_win(board, player):
+            display_board(board)
+            if check_winner(board, player):
                 print(f"Player {player} wins!")
                 return
             if check_tie(board):
+                print("It is a tie!")
                 return
 
 play_game(playing_board)
